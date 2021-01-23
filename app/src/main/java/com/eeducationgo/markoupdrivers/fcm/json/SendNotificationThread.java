32116ef329a71eb7168d0b12ca2fc274eeb804ac
abd <userkhaled>
2021-01-23 16:29:30 +0200

    finish

--- app/src/main/java/com/eeducationgo/markoupdrivers/fcm/json/SendNotificationThread.java
+++ app/src/main/java/com/eeducationgo/markoupdrivers/fcm/json/SendNotificationThread.java
@@ -18,8 +18,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class SendNotificationThread {
-    // TODO: 9/27/2020
-    //  create by eng osama mohamed al-dasoqi
+
 
     private static final String TAG = "SendNotificationThread";
 
